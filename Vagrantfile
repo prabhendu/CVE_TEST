4adf31d9f4406bae47294302b2f8bef2bde74a35
prabhendu <prabhendu@gmail.com>
2015-04-25 20:06:50 -0400

    vm prep

--- Vagrantfile
+++ Vagrantfile
@@ -1,10 +1,12 @@
 # Vagrant Template used for Vagrantfile creation specific to CVE
 
 Vagrant.configure(2) do |config|
-  config.vm.box = "ubuntu/trusty64"
+ config.vm.box = "ubuntu/trusty64"
+
+ config.vm.box_url = "http://iris.hosting.lv/freebsd-10.1-i386.box"
 
   config.vm.provider "virtualbox" do |vb|
-  	vb.memory = "1024"
+  	vb.memory = "512"
   end
 
   config.vm.network "forwarded_port", guest: 80, host: 8080
--- main.py
+++ main.py
@@ -21,9 +21,10 @@ print 'Arguments are :'
 for list in sys.argv:
 	print list
 
-boxname = raw_input('Please enter the name of box :')
-syncfolder = raw_input('Please enter absolute host shared folder name :')
-memory = raw_input('Please enter the memory required :')
+boxname = raw_input('Please enter the name of box : ')
+box_url = raw_input('Please enter the url of box you want : ')
+syncfolder = raw_input('Please enter absolute host shared folder name : ')
+memory = raw_input('Please enter the memory required : ')
 shared_dir = '/vagrant_data'  #local shared dir
 
 # Copying exploit program 
@@ -40,7 +41,7 @@ print('Initializing Vagrant\n')
 vagrant_template = open(syncfolder+'/Vagrantfile','r') #common template
 vagrant_file = open(os.getcwd()+'/Vagrantfile','w') #final actual file
 
-replacements = {'BOX_NAME':boxname , 'HOST_SHARED_DIR':syncfolder , 'GUEST_MOUNT_DIR':shared_dir , 'MEMORY':memory}
+replacements = {'BOX_NAME':boxname , 'HOST_SHARED_DIR':syncfolder , 'GUEST_MOUNT_DIR':shared_dir , 'MEMORY':memory , 'BOX_URL':box_url}
 
 for line in vagrant_template:
 	for src,dest in replacements.iteritems():
@@ -55,34 +56,60 @@ vagrant_file.close()
 #ensure that puppet is installed on Host
 puppet_mod_install = open ('puppet.sh','w')
 
-kernel_deb_dir=raw_input('enter the location of new kernel source(deb files) you want to build:\n ')
-puppet_mod_install.write('cp ' + kernel_deb_dir + '/* ' + syncfolder +'\n')
+new_kernel=raw_input('Do you want to install new kernel(vagrant may have problems later) [y/n]: ')
+if new_kernel == '' or new_kernel == 'n':
+	kernel_deb_dir=''
+	print('Default is no\n')
 
-puppet_list = raw_input('Please enter the puppet modules you want in order\n')
-for module in puppet_list.split():
-        puppet_mod_install.write('puppet module install --force --modulepath ' + os.getcwd() + '/puppet/modules ' + module + '\n' )
+if new_kernel == 'y':
+	kernel_deb_dir=raw_input('enter the location of new kernel source(deb files) you want to build: ')
+	kernel_version=raw_input('enter the kernel version : ')
+
+if kernel_deb_dir != '':
+	puppet_mod_install.write('cp ' + kernel_deb_dir + '/*.deb ' + syncfolder +'\n')
+
+num = raw_input('How many puppet modules you want to install : ')
+for i in range(int(num)):
+	j=0
+	x=raw_input('Enter the module-name and version if any : ')
+	for val in x.split():
+		if j==1:
+			version = val
+		if j==0:
+			module_name = val
+			version = ''
+			j=j+1
+	if version != '':
+		puppet_mod_install.write('puppet module install --modulepath ' + os.getcwd() + '/puppet/modules ' + module_name + ' --version ' + '\"<=' + version + '\"' +'\n' )
+	else:
+		puppet_mod_install.write('puppet module install --modulepath ' + os.getcwd() + '/puppet/modules ' + module_name +'\n' )
+
+#puppet_list = raw_input('Please enter the puppet modules you want in order\n')
+#for module in puppet_list.split():
+#        puppet_mod_install.write('puppet module install --force --modulepath ' + os.getcwd() + '/puppet/modules ' + module + '\n' )
 puppet_mod_install.close()
 #Download puppet modules
 os.chmod("./puppet.sh",0755)
 call("./puppet.sh",shell=True)
 
-#Creating provisioning file
-package_list=raw_input('Please enter list of inorder packages you want in VM: ')
+#Creating shell provisioning file
 prov_file = open(os.getcwd()+'/provision.sh','w')
 prov_file.write('#!/usr/bin/env bash\n')
 prov_file.write('#update the server\n')
 prov_file.write('sudo apt-get update \n')
 prov_file.write('sudo apt-get -y upgrade \n')
-prov_file.write('sudo apt-get -y install dkms build-essential linux-headers-generic vim gcc\n')
+prov_file.write('sudo apt-get -y install dkms build-essential linux-headers-generic vim gcc git puppet-common\n')
 
+package_list=raw_input('Please enter list of sudo packages you want in VM : ')
 for package in package_list.split():
 	prov_file.write('sudo apt-get install -y ' + package + '\n')
 
 #Coying all files from shared directory.Kernel rebuild destroys shared directory
-prov_file.write('sudo mkdir /temp')
+prov_file.write('sudo mkdir /temp\n')
 prov_file.write('sudo cp ' + shared_dir +'/* /temp \n' )
 if kernel_deb_dir != '':
-	prov_file.write('sudo dpkg -i *.deb\n')
+	prov_file.write('sudo apt-get install linux-headers-'+kernel_version+'*\n')
+	prov_file.write('sudo dpkg -i /temp/*.deb\n')
 
 prov_file.close();
 
